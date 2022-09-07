0f0af0000498e30cd7eb7dda7660289a2cf22908
0xantman <antho74830@gmail.com>
2022-09-07 13:37:14 +0200

    First commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['en-US', 'fr', 'nl-NL'],
+    defaultLocale: 'en-US',
+  }
+}
+
+module.exports = nextConfig
